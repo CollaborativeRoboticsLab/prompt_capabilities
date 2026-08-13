c83ee930d4926801a9b7ddeead08e04f48d7d5f9
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-08-13 02:21:12 +0000

    removed get_capability_spec_runner due to merging the process into fabric server

--- src/prompt_runners.cpp
+++ src/prompt_runners.cpp
@@ -3,12 +3,8 @@
 #include <prompt_capabilities/prompt_text_runner.hpp>
 #include <prompt_capabilities/prompt_speech_runner.hpp>
 #include <prompt_capabilities/prompt_pose_runner.hpp>
-#include <prompt_capabilities/prompt_plan_runner.hpp>
-#include <prompt_capabilities/prompt_capability_runner.hpp>
 
 // register runner plugins
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::PromptTextRunner, capabilities2_runner::RunnerBase)
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::PromptSpeechRunner, capabilities2_runner::RunnerBase)
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::PromptPoseRunner, capabilities2_runner::RunnerBase)
-PLUGINLIB_EXPORT_CLASS(capabilities2_runner::PromptPlanRunner, capabilities2_runner::RunnerBase)
-PLUGINLIB_EXPORT_CLASS(capabilities2_runner::PromptCapabilityRunner, capabilities2_runner::RunnerBase)
